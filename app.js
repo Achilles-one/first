e2030049e8ff61d229af9b46f828e421362f07fe
Achilles-one <2276635831@qq.com>
2021-09-03 15:36:03 +0800

    Tech Bees

---
+++ app.js
@@ -0,0 +1,39 @@
+// Scroll to top
+const scrolltp = document.querySelector("#scrolltp");
+scrolltp.addEventListener("click", function () {
+  window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: "smooth",
+  });
+});
+window.addEventListener("scroll", function () {
+  if (window.scrollY >= 700) {
+    scrolltp.style.opacity = 1;
+  } else {
+    scrolltp.style.opacity = 0;
+  }
+});
+
+// Theme
+
+const themeToggle = document.querySelector(".checkbox");
+const body = document.querySelector("body");
+const darkmode = localStorage.getItem("dark");
+
+if (darkmode) {
+  body.classList.add("dark");
+  themeToggle.checked = true;
+}
+
+themeToggle.addEventListener("change", function () {
+  body.classList.toggle("dark");
+
+
+//设置本地存储，记忆之前选择的模式
+  if (body.classList.contains("dark")) {
+    localStorage.setItem("dark", "active");
+  } else {
+    localStorage.removeItem("dark");
+  }
+});
